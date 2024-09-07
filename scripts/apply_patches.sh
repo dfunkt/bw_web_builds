f409c85cef28fff874e455469102856d6a7ea3dd
dfunkt <daniel.barabasa@gmail.com>
2024-09-08 02:56:53 +0300

    Change default theme to System

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -62,4 +62,10 @@ rm -rf ./apps/web/src/app/tools/access-intelligence/
 echo "Using patch: ${PATCH_NAME}"
 git apply "../patches/${PATCH_NAME}" --reject
 
+echo "Change default theme to System"
+sed -i 's/ThemeType\.Light/ThemeType.System/g' \
+    ./apps/web/src/app/core/core.module.ts
+sed -i 's/theme: \[ThemeType\.Light\]/theme: [ThemeType.System]/' \
+    ./apps/web/src/app/settings/preferences.component.ts
+
 echo "Patching successful!"
